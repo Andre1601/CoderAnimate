955e50c51eb3358bb8f798044ff0e4061d0703c8
Azkai <dev.andreardi@gmail.com>
2022-11-03 17:35:20 +0700

    .

--- src/components/HeaderLogin.js
+++ src/components/HeaderLogin.js
@@ -6,7 +6,7 @@ function HeaderLogin() {
     return (
         <header className="grid grid-cols-[100px_1fr_100px] bg-[#7868E6] h-[8vh] justify-items-center items-center text-2xl text-white" >
             <BiArrowBack/>
-            <h1>CoderAnimate</h1>
+            <h1>CoderAnimate</h1>   
             <GiHamburgerMenu/>
         </header>
     )
