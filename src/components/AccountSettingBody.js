885bddedb0d06ba9fe5c53205169f61759d318fc
Andre1601 <aardiansyah1601@gmail.com>
2022-11-05 09:30:14 +0700

    Menambahkan General setting

--- src/components/AccountSettingBody.js
+++ src/components/AccountSettingBody.js
@@ -4,13 +4,21 @@ import General from "./Setting/General";
 
 function AccountSettingBody() {
   return (
-    <main className=" px-5 sm:px-10 text-[#777777] ">
-      <button className="flex justify-between border-solid border-2 border-[#A5A5A5] focus:border-[#7868E6] rounded-lg py-3 px-3 w-full mt-20">
+    <main className="sm:grid sm:grid-cols-[150px_1fr] sm:gap-x-8 px-5 sm:px-10 text-[#777777] ">
+      <h2 className="hidden sm:block col-span-2 text-4xl my-16 font-semibold" >General</h2>
+      <div className="hidden sm:flex flex-col" >
+        <a tabIndex='0' className="cursor-pointer py-2 focus:font-semibold ">General</a>
+        <a tabIndex='0' className="cursor-pointer py-2 focus:font-semibold">Edit Profile</a>
+        <a tabIndex='0' className="cursor-pointer py-2 focus:font-semibold">Password</a>
+        <a tabIndex='0' className="cursor-pointer py-2 focus:font-semibold">Social Profiles</a>
+      </div>
+
+      <button className="sm:hidden flex justify-between border-solid border-2 border-[#A5A5A5] focus:border-[#7868E6] rounded-lg py-3 px-3 w-full mt-20">
         <h2 className="leading-none">General</h2>
         <BsChevronDown />
       </button>
       <General/>
-      <button className="bg-[#7868E6] px-5 py-2 my-10 text-white rounded-xl float-right">Save Changes</button>
+      <button className="bg-[#7868E6] px-5 py-2 my-10 text-white rounded-xl float-right sm:col-span-2">Save Changes</button>
     </main>  
   );
 }
--- src/components/Setting/General.js
+++ src/components/Setting/General.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function General() {
   return (
-    <div className="flex flex-col my-10" >
-      <label>Username</label>
-      <input className="border-solid border-2 border-[#A5A5A5] focus:border-[#7868E6] rounded-lg py-2 px-3 w-full mt-2" />
-      <label className="mt-7">Email</label>
-      <input className="border-solid border-2 border-[#A5A5A5] focus:border-[#7868E6] rounded-lg py-2 px-3 w-full mt-2" />
+    <div className="flex flex-col my-10 sm:my-0" >
+      <label for="username" >Username</label>
+      <input id="username" className="border-solid border-2 border-[#A5A5A5] focus:border-[#7868E6] rounded-lg py-2 px-3 w-full mt-2" />
+      <label for="password" className="mt-7">Email</label>
+      <input id="password" type="password" className="border-solid border-2 border-[#A5A5A5] focus:border-[#7868E6] rounded-lg py-2 px-3 w-full mt-2" />
     </div>
   );
 }
