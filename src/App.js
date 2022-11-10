beec05dfe111c7a545f869fb177c8db7f72b1f78
Irhamkamil <irhamkamil8322@gmail.com>
2022-11-10 11:17:07 +0700

    penambahan page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import DetailPage from "./pages/DetailPage";
 import RegisterPage from "./pages/RegisterPage";
 import UploadCodePage from "./pages/UploadCodePage";
 import HomeNotLogin from "./pages/HomeNotLogin";
+import SearchPage from "./pages/SearchPage";
 
 function App() {
   return (
     <>
-      <HomeNotLogin />
+      <SearchPage />
     </>
   );
 }
--- src/pages/HomeNotLogin.js
+++ src/pages/HomeNotLogin.js
@@ -5,39 +5,68 @@ import Contents from "../components/Contents";
 
 function HomeNotLogin() {
   return (
-    <main>
+    <>
       <HeaderNotLogin />
-      <div class="flex flex-col container mx-auto p-20 bg-[#B8B5FF] ">
-        <div class="w-full px-4">
-          <h1 class="text-[25px] font-bold text-center">
-            The place where all the css animation <br></br> designs in the world
-          </h1>
-          <h2 class="text-[14px] text-center font-light pt-3">
-            Many expert developers have joined CoderAnimate <br></br> so find
-            thousands of designs here
-          </h2>
-        </div>
-        <div>
-          <form action="" class="relative w-max mx-auto pt-5">
+      <main>
+        <div class="flex flex-col mx-auto py-20 bg-[#B8B5FF] ">
+          <div class="w-full px-4">
+            <h1 class="text-[25px] font-bold text-center">
+              The place where all the css animation <br></br> designs in the
+              world
+            </h1>
+            <h2 class="text-[14px] text-center font-light pt-3">
+              Many expert developers have joined CoderAnimate <br></br> so find
+              thousands of designs here
+            </h2>
+          </div>
+          <form action="" class=" relative w-max mx-auto pt-5">
             <input
               type="search"
               name="search"
               id="search"
-              class="relative bg-transparent rounded-full border cursor-auto outline-none pl-20 h-8
-                        focus:w-full focus:border-[#7868E6] focus:cursor-text focus:pl-20 focus:pr-4"
+              placeholder="s"
+              class="relative bg-white rounded-full border cursor-pointer outline-none pl-60 h-20"
             ></input>
-            <div class="absolute inset-y-0 my-5 stroke-gray-500 py-2 h-20 w-12 px-4 border-r border-transparent peer-focus:border-lime-300 peer-focus:stroke-amber-300">
+            <div class="absolute inset-y-11 inset-x-8 font-semibold text-xl">
+              <p>Search...</p>
+            </div>
+            <div class="absolute inset-y-[53px] inset-x-[380px]">
               <BsSearch />
             </div>
           </form>
+          <div class="py-3 mx-auto flex">
+            <h2 id="latest_search" class="px-4 py-2 font-medium">
+              Latest Search
+            </h2>
+            <div class="px-2 py-1">
+              <button class="text-base bg-white px-3 rounded-full ">
+                <div class="px-4 py-1">css</div>
+              </button>
+            </div>
+            <div class="px-2 py-1">
+              <button class="text-base bg-white px-3 rounded-full ">
+                <div class="px-4 py-1">design</div>
+              </button>
+            </div>
+            <div class="px-2 py-1">
+              <button class="text-base bg-white px-3 rounded-full ">
+                <div class="px-4 py-1">animation</div>
+              </button>
+            </div>
+            <div class="px-2 py-1">
+              <button class="text-base bg-white px-3 rounded-full ">
+                <div class="px-4 py-1">button</div>
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-      <div class="grid lg:grid-cols-3 gap-2 aspect-[4/3] sm:grid-cols-1 md:grid-cols-2">
-        <Contents />
-        <Contents />
-        <Contents />
-      </div>
-    </main>
+        <div class=" grid lg:grid-cols-3 gap-2 aspect-[4/3] sm:grid-cols-1 md:grid-cols-2">
+          <Contents />
+          <Contents />
+          <Contents />
+        </div>
+      </main>
+    </>
   );
 }
 
