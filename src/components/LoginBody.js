90bc147ff079254fde8f53464f66e7b3269cfba8
ahmadmcer <ahmadmcer@gmail.com>
2022-11-10 11:31:45 +0700

    feat: Added Login and Forgot Password pages.

---
+++ src/components/LoginBody.js
@@ -0,0 +1,18 @@
+import React from "react";
+import InputLogin from "./Login/InputLogin";
+
+function LoginBody() {
+  return (
+    <>
+      <main className="mx-8 mt-20 md:m-32">
+        <header>
+          <h1 className="text-4xl mb-4">Welcome Back</h1>
+          <p className="text-gray-500">Welcome back! Please enter your detail.</p>
+        </header>
+        <InputLogin />
+      </main>
+    </>
+  );
+}
+
+export default LoginBody;
---
+++ src/pages/ForgotPassword.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HeaderLoginPage from "../components/HeaderLoginPage";
+import ForgotPasswordBody from "../components/ForgotPasswordBody";
+
+function ForgotPassword() {
+  return (
+    <>
+      <HeaderLoginPage />
+      <ForgotPasswordBody />
+    </>
+  );
+}
+
+export default ForgotPassword;
