a80dc0e0a4d5555aa4fa7f3e0e9f25ab4fbe336e
Albas-tomi <@albustomi679@gmail.com>
2022-11-17 10:12:53 +0700

    Favicon Website

--- src/components/HeaderLogin.js
+++ src/components/HeaderLogin.js
@@ -6,7 +6,7 @@ import {Link} from "react-router-dom";
 function HeaderLogin() {
   return (
     <header className="grid sm:flex grid-cols-[30px_1fr_30px] sm:justify-between px-3 sm:px-10 bg-[#7868E6]  h-16 sm:h-20 justify-items-center items-center text-2xl sm:text-4xl text-white">
-      <button className="sm:hidden" ><BiArrowBack/></button>
+      <button className="sm:hidden"><BiArrowBack/></button>
       <Link to="/"><h1 className="cursor-pointer">CoderAnimate</h1></Link>
       <Drawer/>
     </header>
