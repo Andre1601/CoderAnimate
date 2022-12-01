7ed48d536e150226ec0313681d38c90ac8b69720
Andre1601 <aardiansyah1601@gmail.com>
2022-12-02 01:30:27 +0700

    menambahkan headernotlogin

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -2,13 +2,24 @@ import React from "react";
 import HeaderLogin from "../components/HeaderLogin";
 import FooterPage from "../components/FooterPage";
 import SearchPageBody from "../components/SearchPageBody";
+import HeaderNotLogin from "../components/HeaderNotLogin";
 
-function SearchPage() {
+function SearchPage({ authedUser }) {
+  if (authedUser === null) {
+    return (
+      <>
+        <HeaderNotLogin />
+        <SearchPageBody />
+        <FooterPage />
+      </>
+    );
+  }
+
   return (
     <>
       <HeaderLogin />
-      <SearchPageBody/>
-      <FooterPage/>
+      <SearchPageBody />
+      <FooterPage />
     </>
   );
 }
