cba8134df5fff4e9665ad61aa1c69505ce0ace9e
Andre1601 <aardiansyah1601@gmail.com>
2022-12-07 13:59:14 +0700

    add

--- src/utils/network-data.js
+++ src/utils/network-data.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://orange-slippers.cyclic.app";
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://orange-slippers.cyclic.app";
+// const BASE_URL = "http://localhost:8000";
 
 function getAccessToken() {
   return localStorage.getItem("accessToken");
