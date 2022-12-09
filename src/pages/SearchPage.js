62706cd519e66b6025ffdd3060afb6d5d6cd53e8
Andre1601 <aardiansyah1601@gmail.com>
2022-12-09 16:30:14 +0700

    .

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderLogin from "../components/HeaderLogin";
 import FooterPage from "../components/FooterPage";
 import SearchPageBody from "../components/SearchPageBody";
 import HeaderNotLogin from "../components/HeaderNotLogin";
 import Contents from '../components/Contents';
+import { useSearchParams } from "react-router-dom";
+import { getAllProjects } from "../utils/network-data";
 
 function SearchPage({ authedUser, logout }) {
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const {post, setPost} = useState();
+  const [keyword, setKeyword] = useState(() => {
+    return searchParams.get("keyword") || "";
+  });
+
+  React.useEffect(() => {
+    getAllProjects().then(({ post }) => {
+      setPost(post);
+    });
+  }, []);
+
+  function onKeywordChangeHandler(keyword) {
+    setKeyword(keyword);
+    setSearchParams({ keyword });
+  }
+
+  const searchPost = post.filter((note) =>
+    note.title.toLowerCase().includes(keyword.toLowerCase())
+  );
+
   if (authedUser === null) {
     return (
       <>
